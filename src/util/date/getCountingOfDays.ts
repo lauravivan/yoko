d70c897d781a7f585567b72deaa241c14e4057e0
lauravivan <lauvivang@gmail.com>
2025-12-24 19:58:36 -0300

    :bug: fix: counting of days

--- src/util/date/getCountingOfDays.ts
+++ src/util/date/getCountingOfDays.ts
@@ -1,12 +1,12 @@
 import { differenceInCalendarDays } from "date-fns";
+import { createUTCDateNow } from "./createUTCDate";
 
 export function getCountingOfDays(dateToEvent: Date): string {
-  const currentDate = new Date();
-  const difference = differenceInCalendarDays(dateToEvent, currentDate);
+  const difference = differenceInCalendarDays(dateToEvent, createUTCDateNow());
 
   const happened = difference < 0 && "Already happened";
-  const today = difference == 0 && "It's today!!";
-  const oneDay = difference == 1 && `In ${difference} day`;
+  const today = difference === 0 && "It's today!!";
+  const oneDay = difference === 1 && `In ${difference} day`;
 
   return happened || today || oneDay || `In ${difference} days`;
 }
