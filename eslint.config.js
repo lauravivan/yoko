adaf6c9c687056bd1794a71e29edee757d4f3b97
lauravivan <lauvivang@gmail.com>
2026-06-23 18:22:18 -0300

    chore(eslint.config): adjust ignores

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,17 @@ import sonarjs from 'eslint-plugin-sonarjs';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig(
-  { ignores: ['dist', 'node_modules', '.github', '.husky', 'docs'] },
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      '.github',
+      '.husky',
+      'docs',
+      'vite-env.d.ts',
+      'vite.config.ts',
+    ],
+  },
   {
     extends: [
       js.configs.recommended,
@@ -83,13 +93,5 @@ export default defineConfig(
       ],
     },
   },
-  prettierConfig,
-  globalIgnores([
-    '/node_modules',
-    'dist/**',
-    'eslint.config.js',
-    'vite-env.d.ts',
-    'vite.config.ts',
-    '/docs',
-  ])
+  prettierConfig
 );
