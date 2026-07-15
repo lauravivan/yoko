6284fc418d197bf00c508a2520b672d05db358f5
lauravivan <lauvivang@gmail.com>
2026-07-15 15:56:36 -0300

    style: update CountingCard edit modal styles

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,3 +1,11 @@
 export const CARD_COLORS = {
   babyPink: '#ffd7f6',
+  aqua: '#c6ebee',
+  pink: '#ebc6ee',
+  purple: '#e0bcff',
+  yellow: '#ffe99f',
+  orange: '#ffb494',
+  brown: '#b59c8a',
+  green: '#b5e2ba',
+  red: '#ff615e',
 };
