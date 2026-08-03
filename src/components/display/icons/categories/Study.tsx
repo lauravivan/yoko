42ed5eb0441a5677c4f8f0b40ff7d760f2b72d8e
Laura 🐼 <64754203+lauravivan@users.noreply.github.com>
2026-08-03 12:51:47 -0300

    fix: update svg to JSX (#60)

--- src/components/display/icons/categories/Study.tsx
+++ src/components/display/icons/categories/Study.tsx
@@ -1,41 +1,20 @@
 const StudyIcon = () => (
-  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
     <g
-      id="SVGRepo_tracerCarrier"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    ></g>
-    <g id="SVGRepo_iconCarrier">
-      {' '}
-      <path
-        d="M3.5 18V7C3.5 3 4.5 2 8.5 2H15.5C19.5 2 20.5 3 20.5 7V17C20.5 17.14 20.5 17.28 20.49 17.42"
-        stroke="#292D32"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></path>{' '}
-      <path
-        d="M6.35 15H20.5V18.5C20.5 20.43 18.93 22 17 22H7C5.07 22 3.5 20.43 3.5 18.5V17.85C3.5 16.28 4.78 15 6.35 15Z"
-        stroke="#292D32"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></path>{' '}
-      <path
-        d="M8 7H16"
-        stroke="#292D32"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></path>{' '}
-      <path
-        d="M8 10.5H13"
-        stroke="#292D32"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></path>{' '}
+      stroke="#292D32"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+    >
+      {"{' '}"}
+      <path d="M3.5 18V7c0-4 1-5 5-5h7c4 0 5 1 5 5v10c0 .14 0 .28-.01.42" />
+      {"{' '}"}
+      <path d="M6.35 15H20.5v3.5c0 1.93-1.57 3.5-3.5 3.5H7c-1.93 0-3.5-1.57-3.5-3.5v-.65C3.5 16.28 4.78 15 6.35 15Z" />
+      {"{' '}"}
+      <path d="M8 7h8" />
+      {"{' '}"}
+      <path d="M8 10.5h5" />
+      {"{' '}"}
     </g>
   </svg>
 );
