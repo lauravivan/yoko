de3265fab413a5b468cb49cf1d0ee15ee928d1de
Laura 🐼 <64754203+lauravivan@users.noreply.github.com>
2026-08-04 16:55:19 -0300

    cleanup: remove generateRandomString function (#67)

--- src/helpers/generators/random.ts
+++
@@ -1,9 +0,0 @@
-export const generateRandomString = (actualValue: string, values: string[]) => {
-  let random = values[Math.floor(Math.random() * values.length)];
-
-  while (random == actualValue) {
-    random = values[Math.floor(Math.random() * values.length)];
-  }
-
-  return random;
-};
