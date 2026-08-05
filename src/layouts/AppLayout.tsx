34b1026d6a238de9049e43cfac5fb1bf2c046a1a
lauravivan <lauvivang@gmail.com>
2026-08-05 19:55:13 -0300

    fix: add header items to aside, restore add btn

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -6,6 +6,8 @@ import navigation from '@/navigation';
 import TaskIcon from '@/components/display/icons/features/Task';
 import NoteIcon from '@/components/display/icons/features/Note';
 import MoodHistoryIcon from '@/components/display/icons/features/Mood';
+import SignInIcon from '@/components/display/icons/SignIn';
+import SunnyIcon from '@/components/display/icons/day-period/Sunny';
 
 const AppLayout = () => {
   return (
@@ -14,34 +16,56 @@ const AppLayout = () => {
       <div className="app__main-wrapper">
         <div className="app__main-wrapper__outlet-wrapper">
           <aside className="app__main-wrapper__outlet-wrapper__aside">
-            <ul>
-              <li>
+            <ul className="app__main-wrapper__outlet-wrapper__aside__top">
+              <li title="Home">
+                <Link to="/">
+                  <img src="/mascot.svg" />
+                </Link>
+              </li>
+              <li title="Theme">
+                {/* eslint-disable @typescript-eslint/no-empty-function */}
+                <button
+                  className="app__main-wrapper__outlet-wrapper__aside__top__theme"
+                  type="button"
+                  onClick={() => {}}
+                  aria-label="Switch theme"
+                >
+                  <SunnyIcon />
+                </button>
+                {/* eslint-enable @typescript-eslint/no-empty-function */}
+              </li>
+            </ul>
+            <ul className="app__main-wrapper__outlet-wrapper__aside__middle">
+              <li title="Occurrences">
                 <Link
-                  title="Occurrences"
                   aria-label="Go to occurrences"
                   to={navigation.navigateToOccurrences().pathname}
                 >
                   <ClockIcon />
                 </Link>
               </li>
-              <li>
-                <Link title="Notes" aria-label="Go to notes" to="#">
+              <li title="Notes">
+                <Link aria-label="Go to notes" to="#">
                   <NoteIcon />
                 </Link>
               </li>
-              <li>
-                <Link title="Tasks" aria-label="Go to tasks" to="#">
+              <li title="Tasks">
+                <Link aria-label="Go to tasks" to="#">
                   <TaskIcon />
                 </Link>
               </li>
-              <li>
-                <Link
-                  title="Mood history"
-                  aria-label="Go to mood history"
-                  to="#"
-                >
+              <li title="Mood history">
+                <Link aria-label="Go to mood history" to="#">
                   <MoodHistoryIcon />
                 </Link>
+              </li>
+            </ul>
+            <ul className="app__main-wrapper__outlet-wrapper__aside__bottom">
+              <li title="Sign in">
+                {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
+                <button type="button" onClick={() => {}} aria-label="Sign in">
+                  <SignInIcon />
+                </button>
               </li>
             </ul>
           </aside>
--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -1,9 +1,6 @@
-import { Link } from 'react-router';
 import MoodIcon from '@/components/display/icons/Mood';
 import SearchIcon from '@/components/display/icons/Search';
 import { useRef } from 'react';
-import SunnyIcon from '@/components/display/icons/day-period/Sunny';
-import SignInIcon from '@/components/display/icons/SignIn';
 
 const Header = () => {
   const isMouseDown = useRef(false);
@@ -39,70 +36,38 @@ const Header = () => {
   };
 
   return (
-    <div
+    <header
       className="c-header scale-in-center"
       onMouseDown={handleMouseDown}
       onMouseMove={handleDrag}
       onMouseUp={handleMouseUp}
     >
-      <header className="c-header__header">
-        <div className="c-header__header__left-side">
-          <Link className="c-header__header__left-side__logo-wrapper" to="/">
-            <div className="c-header__header__left-side__logo-wrapper__logo">
-              <div className="c-header__header__left-side__logo-wrapper__logo__img-wrapper">
-                <img src="/mascot.svg" />
-              </div>
-              <div className="c-header__header__left-side__logo-wrapper__logo__appname">
-                Yoko!
-              </div>
-            </div>
-          </Link>
-        </div>
-        <div className="c-header__header__middle">
-          {/* eslint-disable @typescript-eslint/no-empty-function */}
-          <button
-            className="c-header__header__right-side__search"
-            type="button"
-            onClick={() => {}}
-            aria-label="Open mood selector"
-            title="Mood selector"
-          >
-            <MoodIcon />
-          </button>
-          <button
-            className="c-header__header__right-side__theme"
-            type="button"
-            onClick={() => {}}
-            aria-label="Switch theme"
-            title="Theme"
-          >
-            <SunnyIcon />
-          </button>
-        </div>
-        <div className="c-header__header__right-side">
-          <button
-            className="c-header__header__right-side__search"
-            type="button"
-            onClick={() => {}}
-            aria-label="Search"
-            title="Search"
-          >
-            <SearchIcon />
-          </button>
-          <button
-            className="c-header__header__right-side__theme"
-            type="button"
-            onClick={() => {}}
-            aria-label="Sign in"
-            title="Sign in"
-          >
-            <SignInIcon />
-          </button>
-          {/* eslint-enable @typescript-eslint/no-empty-function */}
-        </div>
-      </header>
-      <span className="c-header__period">Good morning</span>
-    </div>
+      <div className="c-header__left-side">
+        <span className="c-header__left-side__period">Good morning</span>
+      </div>
+      <div className="c-header__right-side">
+        {/* eslint-disable @typescript-eslint/no-empty-function */}
+        <button
+          className="c-header__middle__search"
+          type="button"
+          onClick={() => {}}
+          aria-label="Open mood selector"
+          title="Mood selector"
+        >
+          <MoodIcon />
+        </button>
+        <button
+          className="c-header__right-side__search"
+          type="button"
+          onClick={() => {}}
+          aria-label="Search"
+          title="Search"
+        >
+          <SearchIcon />
+        </button>
+        {/* eslint-enable @typescript-eslint/no-empty-function */}
+      </div>
+    </header>
   );
 };
 
