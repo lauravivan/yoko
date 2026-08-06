31ecfbb4ccb8b6a8e4d40ae0affe2895b9618465
Laura 🐼 <64754203+lauravivan@users.noreply.github.com>
2026-08-06 09:49:14 -0300

    feat: add modal close btn (#74)
    * feat: add modal close btn
    
    * style: update occurrence card datetime typo
    
    * fix: switch close icon & adjust modal padding

---
+++ src/components/display/icons/Close.tsx
@@ -0,0 +1,8 @@
+const CloseIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <g stroke="#222" strokeLinecap="round" strokeLinejoin="round">
+      <path d="M18 6 6 18M6 6l12 12" />
+    </g>
+  </svg>
+);
+export default CloseIcon;
